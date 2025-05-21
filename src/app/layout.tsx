0ce654fdadd6b3201de29b0e7ae1a43d06bb6e80
Adarsh Dubey <dubeyadarshmain@gmail.com>
2025-05-22 00:35:03 +0530

    feat: update font styles and theme variables in layout and globals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Inter_Tight } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-	variable: "--font-geist-sans",
+const inter = Inter({
+	variable: "--font-inter",
 	subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+const interTight = Inter_Tight({
+	variable: "--font-inter-tight",
 	subsets: ["latin"],
 });
 
@@ -24,11 +24,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<body className="font-body antialiased">{children}</body>
 		</html>
 	);
 }
